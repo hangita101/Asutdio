440d955ae4b772b78bb5eaa7bc513cfe79a1353f
hangita <hangita101@gmail.com>
2024-05-29 10:01:16 +0545

    Adding Log in and Sign up feature

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('login/',views.login_user.as_view(),name='login'),
+    path('logout/', views.logout_user,name='logout'),
+
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Product
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from django.views import View
+from django.http import HttpRequest
+
 
 # Create your views here.
 def home(request):
     products = Product.objects.all()
     
     return render(request,'home.html',{'products':products})
+
+def about(request):
+    return render(request,'about.html',{})
+
+
+
+class login_user(View):
+    def get(self,request:HttpRequest):
+        return render(request,'login.html',{})
+        
+    def post(self,request:HttpRequest):
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request,username=username,password=password)
+        
+        if user is not None:
+            login(request,user)
+            messages.success(request,("Successifully Logged In"))
+            return redirect('home')
+        else:
+            
+            messages.error(request,("There was an error during logging in "))
+            return redirect('login')
+    
+def logout_user(request):
+    logout(request)
+    messages.success(request,("Logged Out"))
+    return redirect('home')
