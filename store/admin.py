c52b1b3929f6e09ae3478a5999573a837e9a2479
hangita <hangita101@gmail.com>
2024-05-29 08:32:13 +0545

    Adding Size table

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Category,Customer,Order,Product 
+from .models import Category,Customer,Order,Product,Size
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Size)
--- store/models.py
+++ store/models.py
@@ -8,6 +8,8 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
+    class Mets:
+        verbose_name_plural = 'categories'
 
 class Customer(models.Model):
     first_name=models.CharField(max_length=50)
@@ -44,3 +46,15 @@ class Order(models.Model):
     
     def __str__(self):
         return self.product
+
+
+class Size(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    SIZE_CHOICES = [
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+        ('XL', 'Extra Large'),
+    ]
+    sizes = models.CharField(max_length=2, choices=SIZE_CHOICES)
+    count=models.IntegerField(default=0)
